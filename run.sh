07284c537803ee68cbb52b2ad45a2841dee5d769
Alfred Roos <alfred@stensatter.se>
2023-08-08 00:32:55 +0200

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc ../printer/src/Canvas.c ../printer/src/msc.c ./src/renderer.c ./src/shapes.c ./src/main.c -lm
+gcc ./Printer/src/Canvas.c ./Printer/src/msc.c ./src/renderer.c ./src/shapes.c ./src/main.c -lm
 read -p "Press Enter to continue" </dev/tty
 ./a.out
 
